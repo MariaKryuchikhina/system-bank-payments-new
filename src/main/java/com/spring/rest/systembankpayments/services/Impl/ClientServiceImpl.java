cc123db34f52332587b192cf378a4dd3a6097969
mariyakryuchikhina <dino69teerex@gmail.com>
2021-09-23 13:22:06 +0300

    Добавлена настройка логгирования

--- src/main/java/com/spring/rest/systembankpayments/services/Impl/ClientServiceImpl.java
+++ src/main/java/com/spring/rest/systembankpayments/services/Impl/ClientServiceImpl.java
@@ -7,10 +7,12 @@ import com.spring.rest.systembankpayments.mapper.ClientMapper;
 import com.spring.rest.systembankpayments.repositories.ClientRepository;
 import com.spring.rest.systembankpayments.services.ClientService;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
+@Log4j2
 @Service
 @AllArgsConstructor
 public class ClientServiceImpl implements ClientService {
@@ -21,6 +23,7 @@ public class ClientServiceImpl implements ClientService {
     @Override
     @Transactional
     public List<ClientDto> findAll() {
+        log.info("Вывод всех клиентов");
         return clientMapper.clientsToClientDto(clientRepository.findAll());
     }
 
